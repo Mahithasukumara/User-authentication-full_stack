60bd05c22e66616c6aa28fbfbbb8534168b763f2
Mahithasukumara <mahissk2005@gmail.com>
2025-04-27 21:57:57 +0530

    dtabase

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -2,12 +2,12 @@ import mongoose from 'mongoose';
 export async function connect(){
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        const connection=mongoose.connection;
+        const connection=await mongoose.connection;
         connection.on('connected',()=>{
             console.log("MongoDB connected");
         })
         connection.on('error',(err)=>{
-            console.log("Mongodb connection error:  "+err);
+            console.log("Mongodb connection error:  ",err);
         })
 
 
